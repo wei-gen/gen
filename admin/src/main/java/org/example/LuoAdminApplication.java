a76c299596809fb5256403c074b66860bfaa111c
fqb <fqb@halababy.com>
2023-07-21 16:36:04 +0800

    提交

--- admin/src/main/java/org/example/LuoAdminApplication.java
+++ admin/src/main/java/org/example/LuoAdminApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LuoApplication {
+public class LuoAdminApplication {
     public static void main(String[] args) {
-        SpringApplication.run(LuoApplication.class,args);
+        SpringApplication.run(LuoAdminApplication.class,args);
     }
 }
